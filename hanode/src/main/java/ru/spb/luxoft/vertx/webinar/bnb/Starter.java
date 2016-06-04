97309f268e853c3f43b394f1579b448d69f6fe09
Vladimir Krasilschik <bob.the.dyer@gmail.com>
2016-06-04 12:43:15 +0300

    now you can pass regular vertx command line as arguments ti main

--- hanode/src/main/java/ru/spb/luxoft/vertx/webinar/bnb/Starter.java
+++ hanode/src/main/java/ru/spb/luxoft/vertx/webinar/bnb/Starter.java
@@ -5,7 +5,7 @@ import io.vertx.core.Launcher;
 public class Starter {
 
     public static void main(String[] args) {
-        Launcher.main(new String[]{"run", System.getProperty("start.verticle"), System.getProperty("vertx.options")});
+        Launcher.main(args);
     }
 
 }
